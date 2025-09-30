3b90c6774d0332d002fa559399e8215c78789361
J3CBX34\J3CBX34 <cherdsak.sokantat@gmail.com>
2025-09-30 15:30:09 +0700

    change version

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,16 +1,17 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = ">= 20.0.0"
+  version = "19.19.1"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.33"
+  cluster_version = "1.27"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2023_x86_64_STANDARD"
+    ami_type = "AL2_x86_64"
+
   }
 
   eks_managed_node_groups = {
